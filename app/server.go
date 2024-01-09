ad5076e3cf7667a0671a848f9d0a6e01c578ddf4
Manh Tu <xxlaguna93@gmail.com>
2024-01-09 15:48:28 +0800

    response with 200

--- app/server.go
+++ app/server.go
@@ -18,9 +18,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+  defer conn.Close()
+  response := "HTTP/1.1 200 OK\r\n\r\n"
+  _, err = conn.Write([]byte(response))
+  if err != nil {
+    fmt.Println("Error writing response: ", err.Error())
+    os.Exit(1)
+  }
 }
